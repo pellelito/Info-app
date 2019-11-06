2c18e21c00bc0e7d23c4e83a4f82d9744badd9d7
OddGrit <odd.restad@gritacademy.se>
2019-11-06 11:56:58 +0100

    Updated with a bad location API

--- scripts.js
+++ scripts.js
@@ -16,12 +16,21 @@ function success(position) {
   
      	 // var key = '491c7653412532b53f822d087e1bdd26';
 		var Weather = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/491c7653412532b53f822d087e1bdd26/" + Geo.lat + "," + Geo.lng ; //Kallar upp Dark Sky
-		// Hämtar väderinfo i en array
+        var location = "https://eu1.locationiq.com/v1/reverse.php?key=ca9050a76a4af5&lat=" + Geo.lat + "&lon=" + Geo.lng + "&format=json";
+        
+    // Hämtar väderinfo i en array
 		$.getJSON([Weather], function(forecast) {
     	// console.log(forecast);
     	// Ställer in valda värden på HTML sidan
-         $('#location').text(forecast.latitude +", " + forecast.longitude);
-    	$('#temp').text(forecast.currently.apparentTemperature);
-		$('#wind').text(forecast.currently.windSpeed);
+            
+            //$('#location').text(forecast.latitude +", " + forecast.longitude);
+            $.getJSON([location], function(loc){
+                $('#location').text(loc.address.county);
+            });
+            //
+            //$('#location').text(loc.place_id);
+            
+            $('#temp').text((forecast.currently.apparentTemperature - 32)/1.8);
+            $('#wind').text(forecast.currently.windSpeed);
 });
 }
